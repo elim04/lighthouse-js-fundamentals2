25ab5f5cdcb3e32f1b495dd6f310ea2548fc6ce5
Emily Lim <emily.elim04@gmail.com>
2020-12-06 18:04:48 +0000

    iterating over an array with while

--- packing-list.js
+++ packing-list.js
@@ -1,9 +1,22 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"]
 
+//using a for loop: 
+
+/*
 console.log("Kitchen stuff to pack: ");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+*/
 
-// to access only one item, we can look at the specific index number
-packingList[3];
+
+// to access only one item, we can look at the specific index number: packingList[3]
+
+//using a while loop
+
+console.log("Kitchen stuff to pack: ");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
